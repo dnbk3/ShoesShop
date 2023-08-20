97169e961dad648f3660a31b58bbc51b365ee45c
daotrongphuc0 <phucdt504@gmail.com>
2023-08-20 11:54:48 +0700

    +data

---
+++ data/huongdichvu_category.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: huongdichvu
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,_binary '','Giay tre em cao cap','Giay tre em','https://ik.imagekit.io/hg4ywf55g2/giaytreem_bLMLlvftZ.png'),(2,_binary '','Giay the thao nam','Giay nam','https://ik.imagekit.io/hg4ywf55g2/giaythethaonam_GXy5rTn8j.png'),(3,_binary '','Giay the thao nu','Giay nu','https://ik.imagekit.io/hg4ywf55g2/giaythethaonu_Wqp9G-Vpu.png');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-20 11:54:08
---
+++ data/huongdichvu_product.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: huongdichvu
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,1,1,'Hàng Oder, Chất lượng tốt nhất, Loại siêu cấp',2090000,'Giày LV Louis Vuitton siêu cấp Virgil Abloh thiết kế trắng gót vàng','https://ik.imagekit.io/hg4ywf55g2/wsxc1679931848107_3-600x450_-_Copy_PDrBK_7k3.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679931848107_3-600x450_a7vOPI3m6.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679931848107_2-600x450_-_Copy_wqcWyGal4n.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679931848107_0_InqsYhoAw.jpg',2590000,50,40,2),(4,1,1,'Hàng Oder,Chất lượng tốt nhất,Loại siêu cấp',2090000,' Giày bánh mì Sanrio trẻ em nhiều màu','https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_4_XjVgUwoQY.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_2-600x600_X4lESU52v.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_1-600x600_ehEWk1dmG.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_0_3so-hkOUB.jpg',500000,50,30,1),(6,1,2,'Hàng Oder,Chất lượng tốt nhất,Loại siêu cấp',0,' Giày bánh mì Sanrio trẻ em nhiều màu','https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_4_XjVgUwoQY.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_2-600x600_X4lESU52v.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_1-600x600_ehEWk1dmG.jpg,https://ik.imagekit.io/hg4ywf55g2/wsxc1679650934005_0_3so-hkOUB.jpg',800000,5,40,0),(17,2,2,'Giày Luviton',580000,'Giày LV','https://capvirgo.com/wp-content/uploads/2023/05/giay-lv-tre-em-46.jpg',600000,30,42,0),(18,1,566,'5555',55,'555','55',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-20 11:54:07
